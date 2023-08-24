db5775f90fb70a6312331afbf935dc5d4106615c
Mert Fazla <mertfazlacontact@gmail.com>
2023-08-24 19:09:14 +0300

    added marker clustering

--- components/ViewPage/MarkerImages.jsx
+++ components/ViewPage/MarkerImages.jsx
@@ -1,15 +1,38 @@
 import { useEffect, useState } from "react";
-import Marker from "@/components/ViewPage/Marker"
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { MarkerClusterer } from "@googlemaps/markerclusterer";
+import { createRoot } from "react-dom/client"
+import { useRef } from "react";
 
 const MarkerImages = ({ map }) => {
 	const [data, setData] = useState([null])
 	const [loading, setLoading] = useState(true)
 	const router = useRouter();
+	const rootRef = useRef()
 
 	const handleMarkerClick = (markerID) => {
 		router.push(`/posts/${markerID}`);
+	}
+	function handleClusterer() {
+		const markers = data.map((marker) => {
+			const container = document.createElement("div")
+			rootRef.current = createRoot(container)
+			rootRef.current.render(
+				<div className="w-16 h-14 border-2 border-purple-100  rounded-full overflow-hidden text-white relative">
+					<Image src={marker.imageURL} fill alt="image" style={{ objectFit: 'cover', objectPosition: 'center' }} sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" />
+				</div>
+			)
+			const markerElement = new google.maps.marker.AdvancedMarkerView({
+				position: { lat: parseFloat(marker.latitude), lng: parseFloat(marker.longitude) },
+				content: container,
+			});
+			markerElement.addListener("click", () => handleMarkerClick(marker.id))
+			return markerElement
+		})
+		const markerClusterer = new MarkerClusterer({ markers, map });
+		console.log("Clusterer is ready");
+		return markerClusterer;
 	}
 
 	const handleFetch = async () => {
@@ -20,27 +43,11 @@ const MarkerImages = ({ map }) => {
 		setLoading(false)
 	}
 	useEffect(() => {
-		handleFetch()
-	}, [])
-
-	return (
-		<>
-			{!loading && map && Object.entries(data).map(([key, marker]) => (
-				<Marker
-					key={key}
-					map={map}
-					position={{
-						lat: parseFloat(marker.latitude),
-						lng: parseFloat(marker.longitude),
-					}}
-					onClick={() => handleMarkerClick(marker.id)}
-				>
-					<div className="w-16 h-14 border-2 border-purple-100  rounded-full overflow-hidden text-white relative">
-						<Image src={marker.imageURL} fill alt="image" style={{ objectFit: 'cover', objectPosition: 'center' }} sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" />
-					</div>
-				</Marker>
-			))}
-		</>
-	)
+		if (!loading) {
+			const markerClusterer = handleClusterer()
+		} else {
+			handleFetch()
+		}
+	}, [data, loading])
 }
 export default MarkerImages
